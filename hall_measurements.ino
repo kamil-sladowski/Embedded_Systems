a1249b8e5df4d8a5b717620040cdcc9f3b5e6dea
kamilo116 <kamil.sladowski@gmail.com>
2019-04-10 22:46:30 +0200

    timer with simple blinking, hall

--- hall_measurements.ino
+++ hall_measurements.ino
@@ -1,6 +1,11 @@
 
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-currentTime[] = {0,0,0}; // HH:MM:SS
+int time = 60;
+int blinking_mode = 1;
+int need_blinking_string = 0;
+int currentTime[] = {0,0,0}; // HH:MM:SS
 
 
 int watchState = 0; // 0 IDLE, 1 HH,  2 MM
@@ -18,6 +23,12 @@ int pushButtonCounter = 0;
 
 int time = 0;
 int measurements[10000];
+
+
+void setup(void) {
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+}
 
 
 void clear_seconds(){
@@ -141,7 +152,110 @@ void clearSecondsIfAppropiateState(){
 }
 
 
-void manage_state(){ 
+increment_time(){
+	time++;
+}
+
+
+read_maesurement(){
+	int analogHall = analogRead(analogPin); 
+    Serial.println("");
+	Serial.println(analogHall);
+	measurements[time] = analogHall;
+}
+
+String get_blinking_timestamp(){
+    String timestamp = format_digits(h) + ":" + "  " + ":" + format_digits(s);
+  return timestamp;
+}
+
+String get_normal_timestamp(){
+  String timestamp = format_digits(h) + ":" + format_digits(m) + ":" + format_digits(s);
+  return timestamp;
+}
+
+int* format_time(){
+  int* hms = (int *)malloc (sizeof (int) * 3);
+  int h = 0;
+  int m = 0;
+  int s = 0;
+  int t = time;
+  h = t / 3600;
+  t = t % (3600*h);
+  m = t / 60;
+  s = t % (60*m);
+  hms[0] =h;
+  hms[1] =m;
+  hms[2] =s;
+  return hms;
+}
+
+void setup(void) {
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+}
+
+
+String get_blinking_timestamp(int h, int m, int s){
+    String timestamp = two_digit_format(h) + ":" + "  " + ":" + two_digit_format(s);
+  return timestamp;
+}
+
+String get_normal_timestamp(int h, int m, int s){
+  String timestamp = two_digit_format(h) + ":" + two_digit_format(m) + ":" + two_digit_format(s);
+  return timestamp;
+}
+
+String get_timestamp()
+{
+  int *hms = format_time();
+  int h = *hms;
+  int m = *(hms+1);
+  int s = *(hms+2);
+
+  if(need_blinking_string){
+    need_blinking_string = 0;
+    return get_blinking_timestamp(h,m,s);
+  }
+  
+  need_blinking_string = 1;
+  return get_normal_timestamp(h,m,s);
+  
+}
+
+
+String format_digits(int i)
+{
+  if (i < 10)
+  {
+    return String(0) + String(i);
+  }
+  else
+  {
+    return String(i);
+  }
+}
+
+
+void display_on_watch(String timestamp){
+  lcd.setCursor(0, 0);
+  
+  lcd.print("time:");
+  delay(100); 
+  
+  lcd.setCursor(0, 1);
+
+  lcd.print(timestamp);
+  Serial.println(timestamp); 
+}
+
+
+
+
+
+
+
+void manage_states(){ 
 
 	if (getRotation() != 0 || isPushButonPressed()){
 		switch(watchState){
@@ -174,9 +288,10 @@ void manage_state(){
 
 void main(){
 	increment_time();
-	read_mesurement();
-	manage_state();
-	display_result();
+	read_maesurement();
+	manage_states();
+	String timestamp = get_timestamp();
+	display_on_watch(timestamp);
 }
 
 
